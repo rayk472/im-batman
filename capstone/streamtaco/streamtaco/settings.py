78218503159881a7db0bda33b4ab9e82cbdfed4a
rayk472 <102784710+rayk472@users.noreply.github.com>
2022-10-18 19:18:11 -0700

    Working on Links models and views

--- capstone/streamtaco/streamtaco/settings.py
+++ capstone/streamtaco/streamtaco/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'usersystem.apps.UsersystemConfig',
+    'taco.apps.TacoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
